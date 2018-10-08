dce4d1b13c1a43eb58c69de0bc5ff9b9a9d04781
Tomasz Plutecki <plutecki_1111825@spk-ssh-access1.if.uj.edu.pl>
2018-10-08 13:29:03 +0200

    add query

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,23 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String args[]){
+        int a=-2; int b=3; int c=-1;
+        double delta;
+           
+        if(a==0){
+            System.out.println("To nie jest funkcja kwadratowa");
+        }else{
+            delta=b*b-4*a*c;
+            if (delta<0){
+                System.out.println("Brak rozwiązań.");
+            }if(delta==0){
+                System.out.println("Równanie ma 1 pierwiastek, x1 = " + (-b/(2*a)) );
+            }if(delta>0){
+                System.out.println("x1 = " + (-b-Math.sqrt(delta))/(2*a) );
+                System.out.println("x2 = " + (-b+Math.sqrt(delta))/(2*a) );
+            }
+        }
     }
+    
 }
