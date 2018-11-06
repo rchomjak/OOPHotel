1a9d9aec282b7d0337557efd8632ab5ef2c16cfd
RCh <richard.chomjak@gmail.com>
2018-11-06 17:44:53 +0100

    Working on Hotel - singleton

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String args[]){
-        int a=-2; int b=3; int c=-1;
-        double delta;
-           
-        if(a==0){
-            System.out.println("To nie jest funkcja kwadratowa");
-        }else{
-            delta=b*b-4*a*c;
-            if (delta<0){
-                System.out.println("Brak rozwiązań.");
-            }if(delta==0){
-                System.out.println("Równanie ma 1 pierwiastek, x1 = " + (-b/(2*a)) );
-            }if(delta>0){
-                System.out.println("x1 = " + (-b-Math.sqrt(delta))/(2*a) );
-                System.out.println("x2 = " + (-b+Math.sqrt(delta))/(2*a) );
-            }
-        }
+
+        Hotel h1 = new Hotel();
+
+        h1.addRoom(4, LuxuryCategory.Cheap);
+        h1.findRoom(0);
+
+        System.out.println(h1);
+
+        Hotel h2 = new Hotel();
+
+        System.out.println(h2);
+
     }
     
 }
